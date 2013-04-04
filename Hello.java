e6245f636572588df19cb0be2102f900b68e5cd5
Charlene <charlet@thoughtworks.com>
2013-04-04 00:21:47 -0700

    Trying again with Jenkins after updating the repo name

--- Hello.java
+++ Hello.java
@@ -4,5 +4,6 @@ class Hello {
 		System.out.println("I managed to add a remote repository and track it successfully");
 		System.out.println("git remote add --track master origin giturl.com:remoterepo.git");
 		System.out.println("Will now try to build through jenkins");
+		System.out.println("Now going for a second try with Jenkins");
 	}
 }
