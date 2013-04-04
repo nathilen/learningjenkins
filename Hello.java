35ccfc85d7041e95a81f19dcc650f8b31956c581
Charlene <charlet@thoughtworks.com>
2013-04-04 00:14:19 -0700

    Will try to build through Jenkins

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ class Hello {
 		System.out.println("Hello Ubuntu");
 		System.out.println("I managed to add a remote repository and track it successfully");
 		System.out.println("git remote add --track master origin giturl.com:remoterepo.git");
+		System.out.println("Will now try to build through jenkins");
 	}
 }
