fb2d64a9c37cb4b8d7576c62aab5ab4748fa4c18
Charlene <charlet@thoughtworks.com>
2013-04-03 23:24:58 -0700

    Added command to remote track a remote repository in master branch

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 class Hello {
 	public static void main(String [] args) {
 		System.out.println("Hello Ubuntu");
+		System.out.println("I managed to add a remote repository and track it successfully");
+		System.out.println("git remote add --track master origin giturl.com:remoterepo.git");
 	}
 }
