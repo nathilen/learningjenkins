0faae90a172f1698686fe7ec913700df353eddb4
Charlene <charlet@thoughtworks.com>
2013-04-04 01:32:02 -0700

    Modified the setup.sh to output message

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,11 +7,15 @@ src_compile() {
 }
 
 generate_jar() {
+	echo "Will now generate jar file"
 	cd $WORKSPACE/src/main
 	/usr/bin/jar -cvef Hello MyHello.jar *.class &&
 	mv -f MyHello.jar $WORKSPACE/out/production/lib
+	echo "Done generating jar file. Status: SUCCESS"
 }
 
 run_jar() {
+	echo "Now trying to run the jar file"
 	/usr/bin/java -jar $WORKSPACE/out/production/lib/MyHello.jar 
 }
+
